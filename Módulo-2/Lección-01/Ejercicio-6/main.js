519f768c6dbd6ae8705be49676fb6d7a4b3458bc
maria-guerrero <mariadol.gc@gmail.com>
2021-11-29 19:48:33 +0100

    add exercise 6

---
+++ Módulo-2/Lección-01/Ejercicio-6/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const contentOne = document.querySelector('.divElements');
+const content = '<h1>Lorem ipsum</h1><img src="http://via.placeholder.com/350x150"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>';
+contentOne.innerHTML = content;
